bec7e677762cf7b04f8fcb5eab4a9fa82d94b8f9
Sylowie <lowsy26@gmail.com>
2025-09-28 01:10:29 +0800

    partial update

--- src/main/java/Sy.java
+++ src/main/java/Sy.java
@@ -17,37 +17,45 @@ public class Sy {
                 System.out.println("      " + "Bye!");
                 System.out.println("___________________________");
                 break;
-
-            } else if (input.equals("list")) {
-                System.out.println("___________________________");
-                for (int i = 0; i < Tasks.size(); i++) {
-                    System.out.println((i + 1) + ". " + Tasks.get(i)); // i+1 cus initial is 0
-                }
-                System.out.println("___________________________");
-
-            } else if (input.startsWith("mark ")) {
-                int i = Integer.parseInt(input.substring(5).trim());
-                Task n = Tasks.get(i - 1);
-                n.markDone();
-                System.out.println("\n Nice! I've marked this task as done: ");
-                System.out.println(" " + n);
-                System.out.println("___________________________");
-
-            } else if (input.startsWith("unmark ")) {
-                int i = Integer.parseInt(input.substring(7).trim());
-                Task n = Tasks.get(i - 1);
-                n.markUndone();
-                System.out.println("\n OK, I've marked this task as not done yet:");
-                System.out.println(" " + n);
-                System.out.println("___________________________");
-
-            } else {
-                Task t = new Task(input);
-                Tasks.add(t);
-                System.out.println("___________________________");
-                System.out.println("added: " + input);
-                System.out.println("___________________________");
             }
+            handle(input);
         }
+
+    private static void add(Task t) {
+        Task t = new Task(input);
+        Tasks.add(t);
+        System.out.println("___________________________");
+        System.out.println("added: " + input);
+        System.out.println("___________________________");
     }
+
+    private static void list() {
+        System.out.println("___________________________");
+        for (int i = 0; i < Tasks.size(); i++) {
+            System.out.println((i + 1) + ". " + Tasks.get(i)); // i+1 cus initial is 0
+        }
+        System.out.println("___________________________");
+    }
+
+    private static void mark() {
+        int i = Integer.parseInt(input.substring(5).trim());
+        Task n = Tasks.get(i
+                - 1);
+        n.markDone();
+        System.out.println("\n Nice! I've marked this task as done: ");
+        System.out.println(" " + n);
+        System.out.println("___________________________");
+    }
+
+    private static void unmark() {
+        int i = Integer.parseInt(input.substring(7).trim());
+        Task n = Tasks.get(i
+                - 1);
+        n.markUndone();
+        System.out.println("\n OK, I've marked this task as not done yet:");
+        System.out.println(" " + n);
+        System.out.println("___________________________");
+    }
+
+}
 }
